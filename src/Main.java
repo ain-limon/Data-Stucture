a0a9bf6eff9297cfea69af8813b6a7e391b01bef
ain-limon <ainlimon0@gmail.com>
2025-03-31 18:36:33 +0600

    Created class Date and Constructor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Date d = new Date(31,3, 2025);
+    }
+}
+
+class Date {
+    int day;
+    int month;
+    int year;
+
+
+    Date(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
 }
