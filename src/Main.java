a55cf89226d2df2e754441f1413ba6b9a331e48a
ain-limon <ainlimon0@gmail.com>
2025-03-31 18:45:06 +0600

    Created isValid method

--- src/Main.java
+++ src/Main.java
@@ -16,4 +16,30 @@ class Date {
         this.month = month;
         this.year = year;
     }
+
+//    boolean isValidDate(){
+//        boolean
+//
+//
+//        return false;
+//    }
+
+    public boolean isValidDate() {
+        if (year < 1 || month < 1 || month > 12 || day < 1) {
+            return false;
+        }
+        int maxDays = 31;
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            maxDays = 30;
+        }
+        else if (month == 2) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                maxDays = 29;
+            } else {
+                maxDays = 28;
+            }
+        }
+        return day <= maxDays;
+    }
+
 }
